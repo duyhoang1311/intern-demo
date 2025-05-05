f44479b15f4734a74ccd48784cd4636f4b0d1fe6
duyhoang1311 <duyhoang3d2y@gmail.com>
2025-05-05 16:01:18 +0700

    Update logto-config

--- user/logto-config.ts
+++ user/logto-config.ts
@@ -2,12 +2,9 @@ import LogtoClient from "@logto/node";
 
 // Cấu hình kết nối với Logto
 export const logtoConfig = {
-  endpoint: process.env.LOGTO_ENDPOINT || "https://vwal09.logto.app",
+  endpoint: process.env.LOGTO_ENDPOINT || "https://vwal09.logto.app/",
   appId: process.env.LOGTO_APP_ID || "36ljq4yaob83uz2j7cmb4",
   appSecret: process.env.LOGTO_APP_SECRET || "pJN43JmnPQytQLc6NU9zEorSPZqyHZKl",
-  baseUrl: process.env.BASE_URL || "https://demo-1-p9ai.encore.app",
-  cookieSecret: process.env.COOKIE_SECRET || "your-cookie-secret",
-  cookieSecure: process.env.NODE_ENV === "production",
   scopes: ["openid", "profile", "email", "offline_access"],
   resources: [process.env.BASE_URL || "https://demo-1-p9ai.encore.app"],
   postLogoutRedirectUri: `${
